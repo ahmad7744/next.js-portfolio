b5731208da7ef46910e899c0638a1baecc1a1beb
ahmad7744 <ahmedaslam.alphasquad@gmail.com>
2023-08-04 16:16:37 +0500

    new

--- pages/about/index.js
+++ pages/about/index.js
@@ -172,7 +172,7 @@ const About = () => {
 
         </div>
         {/* info */}
-         <motion.div
+        <motion.div
         variants={fadeIn ('left' , 0.4)}
         initial="hidden"
         animate="show"
@@ -192,28 +192,29 @@ const About = () => {
             })}
           </div>
           <div className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start  ">
-            {aboutData[index].info.map((item, itmeIndex)=>{
-              return <div key={itmeIndex}
-              className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
-                {/* title */}
-                <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
-                <div className="hidden md:flex ">-</div>
-                <div>{item.stage}</div>
-                {/* icons */}
-                <div className="flex gap-x-4">
-                  {item.icons?.map ((icon, itmeIndex)=>{
-                    return <div className="text-2xl text-white ">{icon}</div>;
-                  })}
-
+      {aboutData[index].info.map((item, itemIndex) => {
+        return (
+          <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+          {/* title */}
+          <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
+          <div className="hidden md:flex ">-</div>
+          <div>{item.stage}</div>
+          {/* icons */}
+          <div className="flex gap-x-4">
+            {item.icons?.map((icon, iconIndex) => {
+              // Use a more stable key for the icons (e.g., icon name)
+              return (
+                <div key={iconIndex.toString()} className="text-2xl text-white ">
+                  {icon}
                 </div>
-
-
-
-                </div>
-
+              );
             })}
-
           </div>
+        </div>
+      );
+    })}
+ 
+    </div>
         </motion.div>
       </div>
     </div>
